50def2c7ae9d9655af2309b655817b61ea8a89e3
ErgoSchmuck <Jackir93@gmail.com>
2025-07-09 13:43:26 +0000

    changed "profile" to "about"

--- jax/app/(tabs)/_layout.tsx
+++ jax/app/(tabs)/_layout.tsx
@@ -41,9 +41,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="profile"
+        name="about"
         options={{
-          title: 'Profile',
+          title: 'About',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
         />      
